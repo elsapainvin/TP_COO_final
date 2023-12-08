127ff8aa865962cac4159e2b31775093aede52fb
elsapainvin <150165563+elsapainvin@users.noreply.github.com>
2023-12-08 14:42:14 +0100

    Update deformation.cpp

--- deformation.cpp
+++ deformation.cpp
@@ -1,9 +1,16 @@
 #include "deformation.h"
 
+// cette fonction prend une image en entrée, applique une transformation définie par les paramètres param,
+// interpole les valeurs déformées à l'aide de la méthode spécifiée par meth_interpole,
+// et stocke les résultats dans deux matrices de sortie :
+//resDeforme pour les valeurs déformées et resMask pour les masques indiquant la validité des coordonnées déformées.
+
+
 void deformation::deforme(const NRmatrix<double>& Im, NRmatrix<double>& resDeforme, NRmatrix<int>& resMask, VecDoub& param, const interpole& meth_interpole) const{
-	int nbr_ligne=Im.nrows();
+	
+	int nbr_ligne=Im.nrows(); //dimensions de l'image
 	int nbr_colonne=Im.ncols();
-	for (int i=0;i<nbr_ligne;i++){
+	for (int i=0;i<nbr_ligne;i++){ //on parcourt l'image d'entrée
 		for (int j=0;j<nbr_colonne;j++){
 			double iprime, jprime;
 			int valeur_mask;
