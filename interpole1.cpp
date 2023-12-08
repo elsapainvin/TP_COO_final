b810e52d31181214f5297fc2fc6635178b36f1e7
elsapainvin <150165563+elsapainvin@users.noreply.github.com>
2023-12-08 14:16:00 +0100

    Update interpole1.cpp

--- interpole1.cpp
+++ interpole1.cpp
@@ -2,12 +2,17 @@
 #include <iostream>
 #include <math.h> 
 
-/*calculer la valeur d’une image I pour des coordonnées non entières. Interpolation bilinéaire*/
+// Objectif : effectuer une interpolation bilinéaire pour estimer la valeur d'un point dans une image,
+// en utilisant les pixels voisins.
+
+//La fonction prend en entrée une matrice d'image Image de type NRmatrix<double>,
+//les coordonnées x et y du point à interpoler,
+//et un pointeur OK qui est utilisé pour indiquer si l'opération d'interpolation a réussi.
 
  double interpole1::interpolation(const NRmatrix<double>& Image, double x, double y, int* OK) const{
  	int H=Image.nrows();
  	int L=Image.ncols();
- 	if ( (x<0) || (x>H-1) || (y<0) || (y>L-1)){
+ 	if ( (x<0) || (x>H-1) || (y<0) || (y>L-1)){ //verif si (x,y) sont dans le support de l'image
  		*OK=0; // on ne peut pas interpoler car le point n'est pas dans le support de l'image.
  		return 0;
  	}
@@ -16,7 +21,8 @@
 		int i1 , i2 , j1 , j2 ;
 		double dx,dy,dfy,dfx,dfxy,res ;
 	
-		// floor permet d'arrondir valeur inférieure.
+		//On calcule les indices des pixels voisins (i1, j1), (i1, j2), (i2, j1), et (i2, j2) dans la matrice Image.
+		//Les indices sont obtenus en arrondissant vers le bas (floor) les coordonnées (x, y).
 		i1 = floor(x);
 		j1 = floor(y);
 	
@@ -31,13 +37,14 @@
 		i2 = i1+1 ;
 		j2 = j1+1 ;
 
+		//calcul des coefficients d'interpolation
 		dx = x - i1;
 		dy = y - j1;
 		dfx = Image[ i2 ][ j1]-Image[ i1 ][ j1 ];
 		dfy = Image[ i1 ][ j2]-Image[ i1 ][ j1 ];
 		dfxy = Image[ i1 ][ j1]+Image[ i2 ][ j2 ]-Image[ i2 ][ j1 ]-Image[ i1 ][ j2 ];
 	
-		// La valeur de I au point (x,y):
+		// Calcul de la valeur interpolée
 		res = Image[i1 ][ j1 ] + dfx * dx + dfy * dy + dx * dy * dfxy;
 		*OK=1;
 		return res;
