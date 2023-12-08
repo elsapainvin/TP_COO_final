f0591be95b7fb1340a5526ddf4d2091245caf1f6
elsapainvin <150165563+elsapainvin@users.noreply.github.com>
2023-12-08 14:31:48 +0100

    Update interpole2.cpp

--- interpole2.cpp
+++ interpole2.cpp
@@ -4,10 +4,14 @@
 
 /*calculer la valeur d’une image I pour des coordonnées non entières. Interpolation au plus proche voisin*/
 
+
+//La fonction prend en entrée une matrice d'image Image de type NRmatrix<double>,
+//les coordonnées x et y du point à interpoler,
+//et un pointeur OK qui est utilisé pour indiquer si l'opération d'interpolation a réussi.
  double interpole2::interpolation(const NRmatrix<double>& Image, double x, double y, int* OK) const{
  	int H=Image.nrows();
  	int L=Image.ncols();
- 	if ( (x<0) || (x>H-1) || (y<0) || (y>L-1)){
+ 	if ( (x<0) || (x>H-1) || (y<0) || (y>L-1)){ //verif si (x,y) sont dans le support de l'image
  		*OK=0; // on ne peut pas interpoler car le point n'est pas dans le support de l'image.
  		return 0;
  	}
@@ -16,7 +20,9 @@
 		int i1 , i2 , j1 , j2, indice_min, i, j ;
 		double d1, d2, d3, d4, dmin, res ;
 	
-		// floor permet d'arrondir valeur inférieure.
+		
+		//On calcule les indices des pixels voisins (i1, j1), (i1, j2), (i2, j1), et (i2, j2) dans la matrice Image.
+		//Les indices sont obtenus en arrondissant vers le bas (floor) les coordonnées (x, y).
 		i1 = floor(x);
 		j1 = floor(y);
 	
@@ -32,6 +38,7 @@
 		j2 = j1+1 ;
 	
 	
+		// calcul des distances au pixel voisin
 		d1 = sqrt((x-i1)*(x-i1)+(y-j1)*(y-j1));
 		indice_min=1;
 		dmin=d1;
@@ -52,7 +59,8 @@
 		if (d4<dmin){
 			indice_min=4;
 		}
-	
+
+		// selection du pixel le plus proche
 		if (indice_min==1){
 			i=i1;
 			j=j1;
@@ -71,7 +79,7 @@
 		}
 	
 	
-		// La valeur de I au point (x,y):
+		// Récupération de la valeur interpolée:
 		*OK=1;
 		return Image[i][j] ;
 	}
